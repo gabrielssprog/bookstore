280b1b390e775b877aa8f47cef2e73938d9d1619
gabriel <gabrielsantanasilva85@gmail.com>
2022-07-01 17:45:02 -0300

    [new] procfile

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ import { App } from './app'
 const connection = new PrismaClient()
 const app = App.newApp(connection)
 
-app.listen(process.env.PORT, () => console.log('server running...'))
+app.listen(process.env.PORT || 3001, () => console.log('server running...'))
